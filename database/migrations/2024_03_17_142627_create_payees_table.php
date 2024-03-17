fec4f2c5db1f245c4337ed4855b3d52fd2ce7626
Brian Waweru <brianwaweru627@gmail.com>
2024-03-18 00:37:42 +0300

    feat: created migration files

---
+++ database/migrations/2024_03_17_142627_create_payees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payees', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('paybill');
+            $table->string('account_number');
+            $table->integer('till');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payees');
+    }
+};
---
+++ database/migrations/2024_03_17_142847_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('payee_id');
+            $table->string('payment_ref');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('debit');
+            $table->integer('credit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
