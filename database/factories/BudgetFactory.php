96427f1736f5bce75a13e92028797478de4d1c15
Brian Waweru <brianwaweru627@gmail.com>
2024-03-18 00:18:59 +0300

    feat: created BudgetFactory

---
+++ database/factories/BudgetFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Budget>
+ */
+class BudgetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'payee_id' => 1,
+            'name' => fake()->name(),
+            'description' => '',
+            'amount' => 0
+        ];
+    }
+}
