dfe87c6f758986a8e59ff531d43236fb554f54a9
Brian Waweru <brianwaweru627@gmail.com>
2024-04-21 22:39:19 +0300

    feat: created BudhetItem component

---
+++ resources/views/components/budget-item.blade.php
@@ -0,0 +1,6 @@
+{{-- <img class="h-48 aspect-video rounded-sm object-cover object-center dark:bg-gray-500" src="https://source.unsplash.com/random/241x361/?1" alt="Image 1"> --}}
+<div class="h-28 aspect-video bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <p class="m-4 font-normal text-gray-700 dark:text-gray-400">Budget Item Name</p>
+    <p class="m-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">KES 10000</p>
+    {{-- <h5 class="m-2 font-normal text-gray-700 dark:text-gray-400">Percentage</h5> --}}
+</div>
