0751b414c5859b0e4111b8aa1094c05c3196adf0
Brian Waweru <brianwaweru627@gmail.com>
2024-04-21 22:40:36 +0300

    feat: created RecentTransactions component

---
+++ resources/views/components/recent-transactions.blade.php
@@ -0,0 +1,11 @@
+<div id="recent-activity" class="mx-6 mt-6">
+  <h2 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Recent Transactions</h2>
+    <div class="mt-3 h-64 overflow-y-auto">
+      <div class="flex flex-col">
+        <x-transaction-card />
+        <x-transaction-card />
+        <x-transaction-card />
+        <x-transaction-card />
+      </div>
+    </div>
+  </div>
