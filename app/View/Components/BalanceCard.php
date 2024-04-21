31932c89de014895743afc808abbe3c0f95537cf
Brian Waweru <brianwaweru627@gmail.com>
2024-04-22 01:07:37 +0300

    feat: displaying balance from db

--- app/View/Components/BalanceCard.php
+++ app/View/Components/BalanceCard.php
@@ -2,8 +2,10 @@
 
 namespace App\View\Components;
 
+use App\Models\Transaction;
 use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
 
 class BalanceCard extends Component
@@ -21,6 +23,10 @@ class BalanceCard extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.balance-card');
+        $debitTotal = Transaction::where('user_id', Auth::id())->sum('debit');
+        $creditTotal = Transaction::where('user_id', Auth::id())->sum('credit');
+        $balance = $creditTotal - $debitTotal;
+
+        return view('components.balance-card')->with('balance', $balance);
     }
 }
