58b6f26b7f70ee77afb7f677dc58d3802c1bfe0d
Brian Waweru <brianwaweru627@gmail.com>
2024-04-21 22:40:53 +0300

    feat: created TransactionCard component

---
+++ resources/views/components/transaction-card.blade.php
@@ -0,0 +1,7 @@
+<div class="flex flex-row justify-between p-3 mb-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <p class="font-normal text-gray-700 dark:text-gray-400">Transaction Item Name</p>
+    <div class="flex flex-col">
+        <p class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">KES 10000</p>
+        <span class="text-xs text-gray-700 dark:text-gray-400">Date</span>
+    </div>
+</div>
